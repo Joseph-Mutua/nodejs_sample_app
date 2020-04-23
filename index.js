0832a1aa40e87d185686911955805536a44efaaf
Joseph Mutua <Mutuaj793@gmail.com>
2020-04-23 23:38:09 +0300

    Finish up static pages

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var http = require("http");
-var dt = require("./myfirstmodule");
+var url = require("url");
 var fs = require("fs");
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end();
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
   }).listen(8080);
