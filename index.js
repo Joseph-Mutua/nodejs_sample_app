edc1d3f295edbc5742a5de1e6e3c31854eb42327
Joseph Mutua <Mutuaj793@gmail.com>
2020-04-23 23:01:36 +0300

    Add home about pages

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var http = require("http");
 var dt = require("./myfirstmodule");
+var fs = require("fs");
 
-http
-  .createServer(function (req, res) {
+http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("The date and time are currently: " + dt.myDateTime());
     res.end();
-  })
-  .listen(8080);
+  }).listen(8080);
